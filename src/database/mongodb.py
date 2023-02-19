b658fd4d88b164a62204352c8236cae794878b37
bhagwat-chate <bhagwat.chate25@gmail.com>
2023-02-19 10:43:11 +0530

    database operation

---
+++ src/database/mongodb.py
@@ -0,0 +1,19 @@
+import os
+import pymongo
+import certifi
+
+ca = certifi()
+
+class MongodbOperation:
+
+    def __init__(self) -> None:
+        self.client = pymongo.MongoClient(os.getenv("MONGO_DB_KEY"), tlsCAFile=ca)
+        self.db_name = "sensorDB"
+
+    def insert_many(self, collection_name, records: list):
+        self.client[self.db_name][collection_name].insert_many(records)
+
+    def insert(self, collection_name, record):
+        self.client[self.db_name][collection_name].insert_one(record)
+
+
